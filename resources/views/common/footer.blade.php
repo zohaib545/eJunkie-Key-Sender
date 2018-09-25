d18ffc882ef964f38ca622a2a13590d7d3059307
zohaib545 <l134031@lhr.nu.edu.pk>
2018-09-25 01:16:20 +0100

    footer

--- resources/views/common/footer.blade.php
+++ resources/views/common/footer.blade.php
@@ -1,7 +1,3 @@
 <footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        <b>Version</b> 2.4.0
-    </div>
-    <strong>Copyright &copy; 2017-2018 <a target="_blank" href="http://encapsulatech.com">Encpsulate Technologies PVT Ltd.</a>.</strong> All rights
-    reserved.
+    
 </footer>
