b34350caba40bb58f9610566b20a31e77f936ebf
zohaib545 <l134031@lhr.nu.edu.pk>
2018-09-25 01:01:22 +0100

    update keys format

--- resources/views/game_keys.blade.php
+++ resources/views/game_keys.blade.php
@@ -1,21 +1,7 @@
-<!DOCTYPE html>
-<html>
+@foreach($game_keys as $game_key)
+{{$game_key['name']}} Steam Key(s)
+@foreach($game_key['keys'] as $code)
+{{$code[0]}}
+@endforeach
 
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Game Keys</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-</head>
-
-<body>
-    @foreach($game_keys as $game_key)
-    <p><b>{{$game_key['name']}} Steam Key(s)</b></p>
-    @foreach($game_key['keys'] as $code)
-    {{$code[0]}}<br>
-    @endforeach
-    <br>
-    @endforeach
-</body>
-
-</html>
+@endforeach
